7b1275b19e2e31e88c1610bb22d5a1af58b2f6cb
Your Name <ooiguancheng1118@1utar.my>
2025-12-10 10:42:52 +0800

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import "./App.css";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<div>Checkout</div>} />
+    </Routes>
+  );
 }
 
 export default App;
